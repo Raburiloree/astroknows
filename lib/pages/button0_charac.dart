97381d0b18bc59adb3e8f8b98ff66d7a19b39a98
Raburiloree <bea110805@gmail.com>
2026-03-07 00:24:43 +0800

    test change

---
+++ lib/pages/button0_charac.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CharacCustPage extends StatefulWidget {
+  const CharacCustPage({super.key});
+
+  @override
+  State<CharacCustPage> createState() => _CharacCustPageState();
+}
+
+class _CharacCustPageState extends State<CharacCustPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: Colors.black,
+       
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          elevation: 0.0,
+          
+          // Back Arrow
+          leadingWidth: 70,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.lightBlueAccent, 
+                  width: 1,            
+                ),
+              ),
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                ),
+            ),
+          ),
+        ),
+
+        body: Center(
+          child: Text('CHARACTER CUSTOMIZATION',
+          style: TextStyle(
+            fontSize: 25,
+          )
+          ),
+        )
+    );
+  }
+}
---
+++ lib/pages/title.dart
@@ -0,0 +1,159 @@
+import 'package:astroknows/pages/button0_charac.dart';
+import 'package:astroknows/pages/button1_mainmenu.dart';
+import 'package:astroknows/pages/button2_shop.dart';
+import 'package:astroknows/pages/button3_settings.dart';
+import 'package:flutter/material.dart';
+
+
+
+class TitlePage extends StatefulWidget {
+  const TitlePage({super.key});
+
+  @override
+  State<TitlePage> createState() => _TitlePageState();
+}
+
+class _TitlePageState extends State<TitlePage> {
+  int star = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: Colors.black,
+
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          elevation: 0.0, //removes shadow of the appBar
+
+
+          // Character Customization Button
+          leading: GestureDetector(
+            onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CharacCustPage())
+                  );
+            },
+          child: Container(
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(100)
+              ),
+            ),
+          ),
+        ),
+
+
+        // Game/App Title
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: Text(
+                'ASTROKNOWS',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 80,
+                ),
+              ),
+            ),
+
+            //Button 1
+            SizedBox(height: 20), //distance from title
+            SizedBox(
+              height: 50,
+              width: 250,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainMenuPage())
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepOrange,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    ),
+                ),
+              child: Text (
+                'Launch Off',
+                style: TextStyle(
+                  fontSize: 23,
+                )
+                ),
+              ),
+              ),
+
+
+            //Button 2
+            SizedBox(height: 15), //distance from Launch Off
+            SizedBox(
+              height: 50,
+              width: 250,
+              child: ElevatedButton(
+                onPressed: () async {
+                final result = await
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ShopPage(star : star))
+                  );
+
+                  if (result != null) {
+                    setState(() {
+                      star = result;
+                    });
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey.shade900,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    ),
+                ),
+              child: Text (
+                'Shop',
+                style: TextStyle(
+                  fontSize: 23,
+                )
+                ),
+              ),
+              ),
+
+            //Button 3
+            SizedBox(height: 20), //distance from Shop
+            SizedBox(
+              height: 50,
+              width: 250,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SettingsPage())
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey.shade900,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    ),
+                ),
+              child: Text (
+                'Settings',
+                style: TextStyle(
+                  fontSize: 23,
+                )
+                ),
+              ),
+              ),
+
+          ],
+        )
+        );
+  }
+}
+
